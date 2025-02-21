9de92d868e39531e27bb01a27d80bc096b2a991a
iampramod <pramodprasanna17@gmail.com>
2025-02-21 19:56:20 +0530

    Create iam.tf

---
+++ dev/iam.tf
@@ -0,0 +1,12 @@
+module "IAM" {
+  source     = "../module/iam"
+  user_name  = "EKS_user"
+  policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSNetworkingPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSServicePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  ]
+}
