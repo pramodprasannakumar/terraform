b3470a1578e2759bf19739231e5a35e4e23e3cae
iampramod <pramodprasanna17@gmail.com>
2025-02-27 22:17:56 +0530

    Update sonar.tf

--- dev/sonar.tf
+++ dev/sonar.tf
@@ -1,8 +1,8 @@
 module "sonarqube_instance" {
-  source             = "..../module/sonarqube"
+  source             = "/root/terraform/module/sonarqube"
   ami               = "ami-04b4f1a9cf54c11d0"
   instance_type     = "t2.medium"
-  key_name         = "t"
-  security_group_id = "sg-046679e1377fbf5d1"
+  key_name         = "zomatokey"
+  security_group_id = "sg-068a0e728afa2d952"
   instance_name     = "sonarqube_server"
 }
