daf2bcd35539caf7c2a03bf0804d247bd6a49e13
iampramod <pramodprasanna17@gmail.com>
2025-02-27 22:17:03 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,5 +1,5 @@
 module "instance" {
-  source = "../modules" # Path to your module repository
+  source = "/root/terraform/module" # Path to your module repository
 
   ami_id        = "ami-053a45fff0a704a47"
   instance_type = "t2.medium"
