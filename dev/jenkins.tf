c03a2cfd4372f92807e781b393143909072c548d
iampramod <pramodprasanna17@gmail.com>
2025-02-27 22:16:21 +0530

    Update jenkins.tf

--- dev/jenkins.tf
+++ dev/jenkins.tf
@@ -1,8 +1,8 @@
 module "jenkins_instance" {
-  source             = "..../module/instance"
+  source             = "/root/terraform/module/jenkins"
   ami               = "ami-04b4f1a9cf54c11d0"  # Add quotes for string values
   instance_type     = "t2.micro"
-  key_name         = "t"
-  security_group_id = "sg-046679e1377fbf5d1"
+  key_name         = "zomatokey"
+  security_group_id = "sg-068a0e728afa2d952"
   instance_name     = "jenkins_server"
 }
